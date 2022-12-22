f5b8951cf7b99ee972eb977900308e51f611a045
Douuvid <silasiharis@gmail.com>
2022-12-22 15:57:10 +0100

    first push phase 2

---
+++ veille_concurrentielle_Partie_2.py
@@ -0,0 +1,15 @@
+ Phase 2
+# Maintenant que vous avez obtenu les informations concernant un premier livre,
+# vous pouvez essayer de récupérer toutes les données nécessaires pour toute une
+# catégorie d'ouvrages.
+
+# Choisissez n'importe quelle catégorie sur le site de Books to Scrape. Écrivez un
+# script Python qui consulte la page de la catégorie choisie, et extrait l'URL de la
+# page Produit de chaque livre appartenant à cette catégorie.
+# Combinez cela avec le travail que vous avez déjà effectué dans la phase 1 afin
+# d'extraire les données produit de tous les livres de la catégorie choisie, puis écrivez
+# les données dans un seul fichier CSV.
+# Remarque : certaines pages de catégorie comptent plus de 20 livres, qui sont
+# donc répartis sur différentes pages (« pagination »). Votre application doit être
+# capable de parcourir automatiquement les multiples pages si présentes.
+
